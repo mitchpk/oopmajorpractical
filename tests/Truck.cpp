1d4f0ab99a817ecec688038b61276faf73f991f0
cobbvanth <80018012+cobbvanth@users.noreply.github.com>
2021-05-27 12:11:00 +0930

    some package tests

--- tests/Truck.cpp
+++ tests/Truck.cpp
@@ -4,17 +4,23 @@
 #include "../Vehicle.h"
 #include "../Fuel.h"
 #include "Truck.h"
+#include "Package.h"
 
 int main() {
 
 
     // Test Aircraft/vehicles here
     Fuel unleaded = Fuel(2.0f, "Unleaded Fuel 808", Fuel::TRUCK, 3);
+    Package p = Package("a non descript packe", 33);
     Truck monstertruck = Truck("Monster Truck", 333, 3333, 3.3f, 33000, unleaded);
 
     assert(monstertruck.getName() == "Monster Truck");
 
     assert(monstertruck.getFuelCapacity() == 333);
+
+    monstertruck.loadPackage(p);
+
+    assert(monstertruck.getPackages()[0].getWeight() == 33);
 
     return 0;
 }
